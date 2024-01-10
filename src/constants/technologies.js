6a3b942edd1bc17774cb221caf003cdf7981d9f0
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2024-01-09 22:21:36 -0300

    Add new project, refractor technologies

---
+++ src/constants/technologies.js
@@ -0,0 +1,10 @@
+export const TECHNOLOGIES = {
+	JS: { name: "JavaScript", className: "bg-yellow-200 text-yellow-800" },
+	React: { name: "React", className: "bg-blue-200 text-blue-800" },
+	Node: { name: "Node.js", className: "bg-green-200 text-green-800" },
+	Express: { name: "Express", className: "bg-purple-200 text-purple-800" },
+	Tailwind: { name: "Tailwind CSS", className: "bg-sky-200 text-sky-800" },
+	MongoDB: { name: "MongoDB", className: "bg-emerald-200 text-emerald-800" },
+	Git: { name: "Git", className: "bg-orange-200 text-orange-800" },
+	HTML: { name: "HTML & CSS", className: "bg-gray-200 text-gray-800" },
+}
