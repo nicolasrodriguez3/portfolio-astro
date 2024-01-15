ae3778fe65d55eed5ecd5311e164741fc96257b6
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2024-01-14 23:24:10 -0300

    Fix dark mode social buttons

---
+++ src/constants/contact.js
@@ -0,0 +1,15 @@
+export const CONTACT = [
+	{
+		title: "Linkedin",
+		url: "https://www.linkedin.com/in/nirodriguez/"
+	},
+	{
+		title: "Github",
+		url: "https://github.com/nicolasrodriguez3"
+	},
+	{
+		title: "Email",
+		url: "mailto:nicolasrodriguez@outlook.com.ar"
+	},
+
+]
